7cd345aaa887ad99601a334e85d2d3fbe5dc0fda
Tomas Kubica <tkubica12@gmail.com>
2015-09-21 11:28:20 +0200

    Oprava HTML v webtest

--- webtest.go
+++ webtest.go
@@ -11,7 +11,7 @@ func main() {
   p := strconv.Itoa(os.Getpid())
 
   m.Get("/", func() string {
-    return "<h1>Moje PID je " + p + "/<h1>"
+    return "<h1>Moje PID je " + p + "</h1>"
   })
 
   m.Run()
